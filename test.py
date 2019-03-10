c40797bd583448fe7bfa707781c8afee4a6e7e79
Joseph <jsc940@gmail.com>
2019-03-10 13:57:25 -0700

    remove two methods

--- test.py
+++ test.py
@@ -5,11 +5,5 @@ class MyCoolClass(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(),'FOO')
 
-    def test_upperish(self):
-        self.assertEqual('foo'.upper(),'FOS')
-
-    def test_uppersish(self):
-        self.assertEqual('foo'.upper(),'FOO')
-
 if __name__ == '__main__':
     unittest.main()
